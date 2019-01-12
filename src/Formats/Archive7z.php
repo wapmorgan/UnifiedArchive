4972a00bc46d4d96ca5eaf7a0c18f108c21f268c
wapmorgan <wapmorgan@gmail.com>
2019-01-12 21:40:27 +0300

    Add hack for archive7z

--- src/Formats/Archive7z.php
+++ src/Formats/Archive7z.php
@@ -11,7 +11,15 @@ class Archive7z extends \Archive7z\Archive7z
      */
     public static function getBinaryVersion()
     {
-        $binary = static::makeBinary7z();
+        if (method_exists(__CLASS__, 'makeBinary7z'))
+            $binary = static::makeBinary7z();
+        else {
+            // some hack for gemorroj/archive7z 4.x version
+            $seven_zip = new self(null);
+            $binary = $seven_zip->getAutoCli();
+            unset($seven_zip);
+        }
+
         $process = new Process([str_replace('\\', '/', $binary)]);
         $result = $process->mustRun()->getOutput();
         if (!preg_match('~7-Zip (\[[\d]+\] )?(?<version>\d+\.\d+)~i', $result, $version))
