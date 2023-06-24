5292c8c62165fc368f43b73c9e35fece37c030d0
wapmorgan <wapmorgan@gmail.com>
2023-06-24 12:50:06 +0300

    Clean test.

--- tests/ArchivingTest.php
+++ tests/ArchivingTest.php
@@ -27,7 +27,6 @@ class ArchivingTest extends PhpUnitTestCase
 
         $test_archive_filename = WORK_DIR.'/'.$archiveFileName;
 
-        var_dump(UnifiedArchive::prepareForArchiving(['' => FIXTURES_DIR], $test_archive_filename));
         $result = UnifiedArchive::create(['' => FIXTURES_DIR], $test_archive_filename);
         $this->assertValueIsInteger($result);
         $this->assertEquals(5, $result);
