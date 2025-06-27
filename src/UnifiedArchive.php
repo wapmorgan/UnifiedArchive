a6e709e0e14bfa26c47871ea204464d5a0c730ff
Sergey <wapmorgan@gmail.com>
2025-06-28 00:54:50 +0300

    Update UnifiedArchive.php

--- src/UnifiedArchive.php
+++ src/UnifiedArchive.php
@@ -19,7 +19,7 @@ use wapmorgan\UnifiedArchive\Exceptions\UnsupportedOperationException;
  */
 class UnifiedArchive implements ArrayAccess, Iterator, Countable
 {
-    const VERSION = '1.2.0';
+    const VERSION = '1.2.1';
 
     /** @var string Type of current archive */
     protected $format;
